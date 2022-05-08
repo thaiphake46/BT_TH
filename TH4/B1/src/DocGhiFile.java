a13813510271d75062cf7768368b0762dfc57812
John Doe <huythaia123@gmail.com>
2022-05-08 15:32:35 +0700

    upload th4

---
+++ TH4/B1/src/DocGhiFile.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.nio.Buffer;
+import java.util.Scanner;
+
+public class DocGhiFile {
+    public static Scanner sc = new Scanner(System.in);
+    public static void taoFile() throws IOException {
+        File fileDaySo = null;
+        boolean isCreat = false;
+        String tenFile = sc.nextLine();
+//        System.out.printf("%s",tenFile);
+        try{
+            fileDaySo = new File("file\\"+tenFile+".txt");
+            isCreat = fileDaySo.createNewFile();
+            if (isCreat)
+                System.out.print("Da tao file thanh cong!");
+            else
+                System.out.print("Tao file that bai");
+        }
+        catch (Exception e){
+            System.out.print(e);
+        }
+
+    }
+
+    public static void ghiFile() throws IOException{
+        int[] arr = {1,2,3,4,5};
+        String tenFile = sc.nextLine();
+        try {
+            FileWriter fw = new FileWriter("file\\"+tenFile+".txt");
+            BufferedWriter bw = new BufferedWriter(fw);
+            for(int i : arr) {
+                bw.write(i);
+
+            }
+            bw.close();
+            fw.close();
+        } catch(IOException e) {
+            System.out.println(e);
+        }
+    }
+
+    public static void docFile() throws IOException {
+
+    }
+}
---
+++ TH4/B1/src/Main.java
@@ -0,0 +1,8 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        DocGhiFile b1 = new DocGhiFile();
+        b1.ghiFile();
+    }
+}
